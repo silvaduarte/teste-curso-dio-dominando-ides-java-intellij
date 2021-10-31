7a098000a81a2bb63792e6be06ee2f1686cd4ddb
Diego Duarte <diegosilva.dd@gmail.com>
2021-10-31 14:23:00 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Cachorro;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Cachorro Cachorro = new Cachorro();
+
+        System.out.println(Cachorro);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello world! " + (a+b));*/
+    }
+}
